7f6f9821380a9641d2d13edb1e93dfabad995b07
UNDEADgtr <undead_gtr@mail.ru>
2013-02-13 23:31:36 +0300

    Added Hello

---
+++ src/main/java/by/undead/HelloBean.java
@@ -0,0 +1,26 @@
+package by.undead;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import java.io.Serializable;
+
+/**
+ * User: Dzmitry
+ * Date: 13.02.13
+ * Time: 22:08
+ */
+
+@ManagedBean
+@SessionScoped
+public class HelloBean implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- src/main/java/by/undead/Login.java
+++ src/main/java/by/undead/Login.java
@@ -28,5 +28,6 @@ public class Login {
 
     public String doLogin(){
         return "login";
+
     }
 }
